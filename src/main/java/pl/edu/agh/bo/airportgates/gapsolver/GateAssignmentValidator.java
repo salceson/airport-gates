4df9ef95afd35199a643446481770137d54b71b9
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-05-11 12:52:48 +0200

    Changed file template.

--- src/main/java/pl/edu/agh/bo/airportgates/gapsolver/GateAssignmentValidator.java
+++ src/main/java/pl/edu/agh/bo/airportgates/gapsolver/GateAssignmentValidator.java
@@ -3,7 +3,7 @@ package pl.edu.agh.bo.airportgates.gapsolver;
 import pl.edu.agh.bo.airportgates.model.GateAssignmentResult;
 
 /**
- * Created by Michal Janczykowski on 2015-05-05.
+ * @author Michał Janczykowski
  */
 public interface GateAssignmentValidator {
     boolean isValid(GateAssignmentResult gapResult);
